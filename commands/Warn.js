debc4afff282b160245e10ca6021b80ded3b8aff
cutibabah <dilshanrashmika128@gmail.com>
2024-06-06 20:54:07 +0530

    Update and rename Warn.js

--- commands/Warn.js
+++ commands/Warn.js
@@ -1,12 +1,12 @@
 /** 
 
-GOLD-MD
+RUSI-MD
   𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
  𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
  𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
  𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
- * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : GOLD 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
- * @𝗼𝘄𝗻𝗲𝗿: UMAR 
+ * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : RUSI 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
+ * @𝗼𝘄𝗻𝗲𝗿: RUSH 
  
  **/
 
@@ -29,29 +29,29 @@ france(
     },async (dest,zk,commandeOptions) => {
 
  const {ms , arg, repondre,superUser,verifGroupe,verifAdmin , msgRepondu , auteurMsgRepondu} = commandeOptions;
-if(!verifGroupe ) {repondre('Yeh command sirf groups me use karo') ; return};
+if(!verifGroupe ) {repondre('මේ command එක group වලට වැඩ කරන්නෙ 📸') ; return};
 
 if(verifAdmin || superUser) {
-   if(!msgRepondu){repondre('Ap kis member ko warnig dena chahte hai usko tag karo ya mention karo'); return};
+   if(!msgRepondu){repondre('mention කල යුතුයි'); return};
    
    if (!arg || !arg[0] || arg.join('') === '') {
     await ajouterUtilisateurAvecWarnCount(auteurMsgRepondu)
    let warn = await getWarnCountByJID(auteurMsgRepondu)
    let warnlimit = s.WARN_COUNT
    
-   if( warn >= warnlimit ) { await repondre('Is member ki warnigs khatam hui , to mene remove ker dya');
+   if( warn >= warnlimit ) { await repondre('Is member warnigs , to mene remove ');
                 zk.groupParticipantsUpdate(dest, [auteurMsgRepondu], "remove")
  } else { 
 
     var rest = warnlimit - warn ;
-     repondre(`Is member ko mene 1 warning de di hai baki warnings reh gayi :• ${rest} `)
+     repondre(`Is member 1 warnings  :• ${rest} `)
    }
 } else if ( arg[0] === 'reset') { await resetWarnCountByJID(auteurMsgRepondu) 
 
-    repondre("Is Bande ki warnigs reset ho gayi hai")} else ( repondre('Jis member ko ap ne pehle warning di thi usko tag ya mention karo likho :• .warn reset'))
+    repondre("Is Bande warnigs reset ")} else ( repondre('member warning  tag  mention  :• .warn reset'))
    
 }  else {
-    repondre('Yeh cmnd sirf group admins use kar sakte hai ap admin nahi ho')
+    repondre(' group admins use  admin ')
 }
  
    });
