c711947dd19d435e1f66b79ef72b696130cd1d03
Rush⭐⭐ <dilshanrashmika128@gmail.com>
2024-06-06 23:20:32 +0530

    Update repo.js

--- commands/repo.js
+++ commands/repo.js
@@ -1,28 +1,45 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "repo", reaction: "🇱🇰", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-    console.log("Commande saisie !!!s");
-    let z = '*RUSi-MD* releace date 2024 jule 6, by *rush* \n\n ' + "FOLLOW MY WHATSAPP CHANNLE🇱🇰 https://whatsapp.com/channel/0029VacPdLXJ93wP6q0b962J
-🇱🇰Then followe my whatsapp couminuty ⭐https://chat.whatsapp.com/IZdyenJpAxM3JB8kSboR19 🎗️devoloper by and. Powered by *RUSI-MD.*";
-    let d = ' ';
-    let varmess = z + d;
-    var img = 'https://telegra.ph/file/f9b9d372441bc2531273b.mp4';
-    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
-    //console.log("montest")
-});
-console.log("mon test");
-/*module.exports.commande = () => {
-  var nomCom = ["test","t"]
-  var reaction="🇱🇰 "
-  return { nomCom, execute,reaction }
-};
+/** 
 
-async function  execute  (origineMessage,zok) {
-  console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *RUSI-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par * Rush*'
-      let varmess=z+d
-      var img='https://telegra.ph/file/f9b9d372441bc2531273b.mp4'
-await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
-}  */ 
+𝗥𝗨𝗦𝗜-𝗠𝗗
+
+  𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
+ 𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
+ 𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
+ 𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
+ * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : 𝗥𝗨𝗦𝗜 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
+ * @𝗼𝘄𝗻𝗲𝗿: 𝗥𝗨𝗦𝗛 
+ 
+ **/
+
+
+
+
+
+
+
+
+
+
+
+const { france } = require("../framework/france");
+const moment = require("moment-timezone");
+const { default: axios } = require('axios');
+//const conf = require('../set');
+
+
+france({ nomCom: 'repo',
+    desc: 'To check repo',
+    Categorie: 'General',
+    reaction: '👑', 
+    fromMe: 'true', 
+
+       
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+    const { start} = new Date().getTime()
+    return repondre('*🇱🇰𝗥𝗨𝗦𝗜-MD🇱🇰https://github.com/cutibabah/rusi-md🇱🇰it🇱🇰ise⭐link*\n ```' + 10 + '``` *|✅|*') 
+    const { end } = new Date().getTime()
+    await zok.sendMessage('*🇱🇰𝗥𝗨𝗦𝗜-MD🇱🇰*\n ```' + (end - start) + '``` *|2x•SPEED|*')
+  }
+)
