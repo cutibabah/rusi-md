db0c73ee522f70ae209ff8b14c0b9cb04d748134
cutibabah <dilshanrashmika128@gmail.com>
2024-06-06 10:23:54 +0530

    Update and rename YouTube (1).js to YouTube.js

--- commands/YouTube.js
+++ commands/YouTube.js
@@ -1,13 +1,13 @@
 /** 
 
-🇫‌🇱‌🇦‌🇸‌🇭‌-🇲‌🇩‌ 
+𝗥𝗜𝗦𝗜-𝗠𝗗 
 
   𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
  𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
  𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
  𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
- * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : 𝗙𝗹𝗮𝘀𝗵 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
- * @𝗼𝘄𝗻𝗲𝗿: 𝗙𝗿𝗮𝗻𝗰𝗲 𝗞𝗶𝗻𝗴 
+ * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : 𝗥𝗨𝗦𝗜 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
+ * @𝗼𝘄𝗻𝗲𝗿: 𝗥𝗨𝗦𝗛 
  
  **/
 
@@ -28,12 +28,12 @@ const yts1 = require("youtube-yts");
 france({
   nomCom: "play",
   categorie: "Search",
-  reaction: "🔰"
+  reaction: "⭐"
 }, async (origineMessage, zk, commandeOptions) => {
   const { ms, repondre, arg } = commandeOptions;
      
   if (!arg[0]) {
-    repondre("Audio song download karne ke lie Song ka name likho");
+    repondre("Audio song 𝗱𝗼𝘄𝗻𝗹𝗼𝗮𝗱 වෙමින්");
     return;
   }
 
@@ -47,11 +47,11 @@ france({
           
        let infoMess = {
           image: {url : videos[0]. thumbnail},
-         caption : `\n*🔰NAME :* _${videos[0].title}_
+         caption : `\n*⭐NAME :* _${videos[0].title}_
 
-*🔰TIME :* _${videos[0].timestamp}_
-*🔰LINK :* _${videos[0].url}_
-*🔰DOWNLOADING...*\n\n`
+*⭐TIME :* _${videos[0].timestamp}_
+*⭐LINK :* _${videos[0].url}_
+*⭐DOWNLOADING...*\n\n`
        }
 
       
@@ -99,7 +99,7 @@ france({
 france({
   nomCom: "video",
   categorie: "Search",
-  reaction: "🔰"
+  reaction: "⭐"
 }, async (origineMessage, zk, commandeOptions) => {
   const { arg, ms, repondre } = commandeOptions;
 
@@ -118,10 +118,10 @@ france({
 
       let InfoMess = {
         image: { url: videos[0].thumbnail },
-        caption: `*🔰NAME :* _${Element.title}_
-*🔰TIME :* _${Element.timestamp}_
-*🔰LINK :* _${Element.url}_
-*🔰DOWNLOADING...*\n\n`
+        caption: `*⭐NAME :* _${Element.title}_
+*⭐TIME :* _${Element.timestamp}_
+*⭐LINK :* _${Element.url}_
+*⭐DOWNLOADING...*\n\n`
       };
 
       zk.sendMessage(origineMessage, InfoMess, { quoted: ms });
@@ -142,7 +142,7 @@ france({
 
       fileStream.on('finish', () => {
         // Envoi du fichier vidéo en utilisant l'URL du fichier local
-        zk.sendMessage(origineMessage, { video: { url :"./video.mp4"} , caption: "*🔰 BY || GOLD || MD 🔰*", gifPlayback: false }, { quoted: ms });
+        zk.sendMessage(origineMessage, { video: { url :"./video.mp4"} , caption: "*⭐ BY || RUSI || MD ⭐*", gifPlayback: false }, { quoted: ms });
       });
 
       fileStream.on('error', (error) => {
