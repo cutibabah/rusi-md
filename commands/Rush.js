39f74356cc25674d7ab829e5041f86d948803520
Rush⭐⭐ <dilshanrashmika128@gmail.com>
2024-06-07 00:15:08 +0530

    Create Rush.js

---
+++ commands/Rush.js
@@ -0,0 +1,14 @@
+});
+
+france({
+  nomCom: "rush",
+  categorie: "General",
+  reaction: "🎗️"
+}, async (origineMessage, zk, commandeOptions) => {
+  const { ms, repondre, arg } = commandeOptions;
+
+  if (!arg[0]) {
+    repondre("I AM RUSH CREATER ගේ online robot 🖥️ rush විසින් මෙය සංවර්දනය කරන තෙක් ඉන්න ");
+    return;
+  }
+
