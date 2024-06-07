9e6aee8fdc5a0ae4c8a4529035e6cf95b0e12735
Rush⭐⭐ <dilshanrashmika128@gmail.com>
2024-06-07 08:33:04 +0530

    Create rusi.js

---
+++ commands/rusi.js
@@ -0,0 +1,117 @@
+
+/** 
+
+             • RUSI MD WHATSAPP BOT •
+
+  𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
+ 𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
+ 𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
+ 𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
+ * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : RUSI 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
+ * @𝗼𝘄𝗻𝗲𝗿: RUSH 
+ 
+ **/
+
+
+
+
+const { france } = require("../framework/france");
+const {getAllSudoNumbers,isSudoTableNotEmpty} = require("../bdd/sudo")
+const conf = require("../set");
+
+france({ nomCom: "user", categorie: "General", reaction: "⭐" }, async (dest, zk, commandeOptions) => {
+    const { ms , mybotpic } = commandeOptions;
+    
+  const thsudo = await isSudoTableNotEmpty()
+
+  if (thsudo) {
+     let msg = `*RUSI-MD USER*\n
+     *🔥 OWNER NUMBER 🔥*\n :
+- ⭐ @${conf.NUMERO_OWNER}
+
+------ *♥️ NUMBERS ♥️* -----\n`
+     
+ let sudos = await getAllSudoNumbers()
+
+   for ( const sudo of sudos) {
+    if (sudo) { // Vérification plus stricte pour éliminer les valeurs vides ou indéfinies
+      sudonumero = sudo.replace(/[^0-9]/g, '');
+      msg += `- 🤍 @${sudonumero}\n`;
+    } else {return}
+
+   }   const ownerjid = conf.NUMERO_OWNER.replace(/[^0-9]/g) + "@s.whatsapp.net";
+   const mentionedJid = sudos.concat([ownerjid])
+   console.log(sudos);
+   console.log(mentionedJid)
+      zk.sendMessage(
+        dest,
+        {
+          image : { url : mybotpic() },
+          caption : msg,
+          mentions : mentionedJid
+        }
+      )
+  } else {
+    const vcard =
+        'BEGIN:VCARD\n' + // metadata of the contact card
+        'VERSION:3.0\n' +
+        'FN:' + conf.OWNER_NAME + '\n' + // full name
+        'ORG:undefined;\n' + // the organization of the contact
+        'TEL;type=CELL;type=VOICE;waid=' + conf.NUMERO_OWNER + ':+' + conf.NUMERO_OWNER + '\n' + // WhatsApp ID + phone number
+        'END:VCARD';
+    zk.sendMessage(dest, {
+        contacts: {
+            displayName: conf.OWNER_NAME,
+            contacts: [{ vcard }],
+        },
+    },{quoted:ms});
+  }
+});
+
+france({ nomCom: "owner", categorie: "General", reaction: "⭐" }, async (dest, zk, commandeOptions) => {
+    const { ms, mybotpic } = commandeOptions;
+
+    const devs = [
+      { nom: "|UMAR|", numero: "94762498519" },
+     { nom: "|NABEEL|", numero: "94762498519" },
+      // Ajoute d'autres développeurs ici avec leur nom et numéro
+    ];
+
+    let message = "⭐ RUSI-MD OWNERS ⭐:\n\n";
+    for (const dev of devs) {
+      message += `----------------\n• ${dev.nom} : wa.me/${dev.numero}\n`;
+    }
+  var lien = mybotpic()
+    if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:message }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:message }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    repondre(lien)
+    repondre("link error");
+    
+}
+});
+
+france({ nomCom: "privacy", categorie: "General" }, async (dest, zk, commandeOptions) => {
+  const { ms, repondre, auteurMessage, } = commandeOptions; 
+ 
+  repondre("*⭐ RUSI-MD ⭐* \n\n\n _ RUSI-MD  OFFICIAL WHATSAPP NOT ALOWED THIS COMMADE ENTER YOUR VALID CODE_")
+  await zk.sendMessage(auteurMessage,{text : `wa.me//762498519?text=HI_Rusi_MD_CONTACT`},{quoted :ms})
+
+})
